3020cafe5f03c0f73c96aa1bf25897a1bf73792d
Adriano Santos <sleipnir@bsd.com.br>
2022-05-15 10:51:03 -0300

    Fix some bugs

--- lib/spawn/actors/actor_entity.ex
+++ lib/spawn/actors/actor_entity.ex
@@ -25,7 +25,7 @@ defmodule Eigr.Functions.Protocol.Actors.ActorEntity do
       ) do
     Logger.debug("Activating actor #{name}")
     Process.flag(:trap_exit, true)
-    Process.send_after(self(), :snapshot, get_snapshot_interval(snapshot_strategy))
+    snapshot(snapshot_strategy)
     Process.send_after(self(), :deactivate, get_deactivate_interval(deactivate_strategy))
 
     {:ok, state, {:continue, :load_state}}
@@ -34,16 +34,49 @@ defmodule Eigr.Functions.Protocol.Actors.ActorEntity do
   @impl true
   @spec handle_continue(:load_state, Eigr.Functions.Protocol.Actors.Actor.t()) ::
           {:noreply, Eigr.Functions.Protocol.Actors.Actor.t()}
+  def handle_continue(:load_state, %Actor{actor_state: nil} = state) do
+    Logger.debug("Initial state is empty... Getting state from state manager.")
+
+    {:ok, current_state} = StateManager.load(state.name)
+    {:noreply, %Actor{state | actor_state: current_state}}
+  end
+
   def handle_continue(:load_state, %Actor{actor_state: %ActorState{} = actor_state} = state) do
-    _current_state = StateManager.load(state.name)
+    Logger.debug(
+      "Initial state is not empty... Trying to reconcile the state with state manager."
+    )
+
+    {:ok, _current_state} = StateManager.load(state.name)
+
+    # TODO: Check if the state is empty in the state manager. If so, then set state from initial state.
     updated_state = actor_state
     {:noreply, %Actor{state | actor_state: updated_state}}
   end
 
   @impl true
-  def handle_info(:snapshot, %Actor{name: name, actor_state: %ActorState{} = actor_state} = state) do
+  def handle_info(
+        :snapshot,
+        %Actor{
+          name: _name,
+          snapshot_strategy: %ActorSnapshotStrategy{strategy: snapshot_strategy},
+          actor_state: nil
+        } = state
+      ) do
+    snapshot(snapshot_strategy)
+    {:noreply, state}
+  end
+
+  def handle_info(
+        :snapshot,
+        %Actor{
+          name: name,
+          snapshot_strategy: %ActorSnapshotStrategy{strategy: snapshot_strategy},
+          actor_state: %ActorState{} = actor_state
+        } = state
+      ) do
     Logger.debug("Snapshotting actor #{name}")
     StateManager.save(name, actor_state)
+    snapshot(snapshot_strategy)
     {:noreply, state}
   end
 
@@ -68,6 +101,9 @@ defmodule Eigr.Functions.Protocol.Actors.ActorEntity do
   def start_link(%Actor{name: name} = actor) do
     GenServer.start(__MODULE__, actor, name: via(name))
   end
+
+  defp snapshot(snapshot_strategy),
+    do: Process.send_after(self(), :snapshot, get_snapshot_interval(snapshot_strategy))
 
   defp get_snapshot_interval(%TimeoutStrategy{timeout: timeout} = _timeout_strategy)
        when is_nil(timeout),
