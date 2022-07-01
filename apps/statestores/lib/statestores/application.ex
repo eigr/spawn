7b8d010c187daeabce727a4b2db56d4ed85d1620
Adriano Santos <sleipnir@bsd.com.br>
2022-07-01 20:17:21 -0300

    Refactor. Code style

--- apps/statestores/lib/statestores/application.ex
+++ apps/statestores/lib/statestores/application.ex
@@ -2,23 +2,18 @@ defmodule Statestores.Application do
   @moduledoc false
 
   use Application
+  import Statestores.Util, only: [load_repo: 0]
 
   @impl true
   def start(_type, _args) do
-    type = String.to_existing_atom(System.get_env("PROXY_DATABASE_TYPE", "mysql"))
     Statestores.Migrator.migrate()
 
-    children = get_supervisor_tree(type)
+    children = [
+      Statestores.Vault,
+      load_repo()
+    ]
 
     opts = [strategy: :one_for_one, name: Statestores.Supervisor]
     Supervisor.start_link(children, opts)
-  end
-
-  defp get_supervisor_tree(:mysql) do
-    [Statestores.Vault, Statestores.Adapters.MySQL]
-  end
-
-  defp get_supervisor_tree(:postgres) do
-    [Statestores.Vault, Statestores.Adapters.Postgres]
   end
 end
