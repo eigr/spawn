4ad8e713adb19b0aff2362cede631bc2394b9b64
Adriano Santos <sleipnir@bsd.com.br>
2022-09-09 19:06:59 -0300

    Added Cloudevents Proto

--- apps/protos/compile-pb.sh
+++ apps/protos/compile-pb.sh
@@ -7,3 +7,5 @@ set -o pipefail
 protoc --elixir_out=gen_descriptors=true:./lib/google/protobuf --proto_path=priv/protos/google/protobuf priv/protos/google/protobuf/any.proto
 protoc --elixir_out=gen_descriptors=true:./lib/actors/actor --proto_path=priv/protos/eigr/functions/protocol/actors priv/protos/eigr/functions/protocol/actors/actor.proto
 protoc --elixir_out=gen_descriptors=true:./lib/actors/actor --proto_path=priv/protos/eigr/functions/protocol/actors priv/protos/eigr/functions/protocol/actors/protocol.proto
+
+protoc --elixir_out=gen_descriptors=true:./lib/cloudevents --proto_path=priv/protos/io/cloudevents/v1 priv/protos/io/cloudevents/v1/spec.proto
