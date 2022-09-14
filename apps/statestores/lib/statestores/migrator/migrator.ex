97816af21dc8db8ce6b2699059977c478b7913ab
Adriano Santos <sleipnir@bsd.com.br>
2022-09-14 10:08:24 -0300

    Add specs

--- apps/statestores/lib/statestores/migrator/migrator.ex
+++ apps/statestores/lib/statestores/migrator/migrator.ex
@@ -1,6 +1,7 @@
 defmodule Statestores.Migrator do
   import Statestores.Util, only: [load_app: 0, load_repo: 0]
 
+  @spec migrate :: {:ok, any, any}
   def migrate do
     load_app()
     repo = load_repo()
@@ -8,6 +9,7 @@ defmodule Statestores.Migrator do
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
   end
 
+  @spec rollback :: {:ok, any, any}
   def rollback(repo, version) do
     load_app()
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
