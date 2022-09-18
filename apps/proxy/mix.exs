8d97315c46e63f5092f2e8f3f0708f959f250153
Adriano Santos <sleipnir@bsd.com.br>
2022-09-18 12:24:02 -0300

    Added some monitoring applications

--- apps/proxy/mix.exs
+++ apps/proxy/mix.exs
@@ -20,7 +20,7 @@ defmodule Proxy.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :runtime_tools, :os_mon],
       mod: {Proxy.Application, []}
     ]
   end
