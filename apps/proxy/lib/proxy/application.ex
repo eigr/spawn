0d6af74f1716e1031c081956b6ca8f479cb5728f
Adriano Santos <sleipnir@bsd.com.br>
2022-09-22 19:29:58 -0300

    Added HTTP Server UDS support

--- apps/proxy/lib/proxy/application.ex
+++ apps/proxy/lib/proxy/application.ex
@@ -16,10 +16,31 @@ defmodule Proxy.Application do
       Statestores.Supervisor.child_spec(),
       Actors.Supervisors.ProtocolSupervisor.child_spec(config),
       Actors.Supervisors.EntitySupervisor.child_spec(config),
-      {Bandit, plug: Proxy.Router, scheme: :http, options: [port: config.http_port]}
+      {Bandit, plug: Proxy.Router, scheme: :http, options: get_http_options(config)}
     ]
 
     opts = [strategy: :one_for_one, name: Proxy.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp get_http_options(config) do
+    if config.proxy_uds_enable == "true" do
+      get_uds_options(config)
+    else
+      get_tcp_options(config)
+    end
+  end
+
+  defp get_uds_options(config) do
+    [
+      port: 0,
+      transport_options: [ip: {:local, config.proxy_sock_addr}]
+    ]
+  end
+
+  defp get_tcp_options(config) do
+    [
+      port: config.http_port
+    ]
   end
 end
