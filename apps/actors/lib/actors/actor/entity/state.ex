5846d9d655b1599d917b3ef596bac36ac4653196
Adriano Santos <sleipnir@bsd.com.br>
2022-09-23 19:52:33 -0300

    Added options to actor entities

--- apps/actors/lib/actors/actor/entity/state.ex
+++ apps/actors/lib/actors/actor/entity/state.ex
@@ -1,11 +1,12 @@
 defmodule Actors.Actor.Entity.EntityState do
   alias Eigr.Functions.Protocol.Actors.Actor
 
-  defstruct system: nil, actor: nil, state_hash: nil
+  defstruct system: nil, actor: nil, state_hash: nil, opts: []
 
   @type t :: %__MODULE__{
           system: String.t(),
           actor: Actor.t(),
-          state_hash: binary()
+          state_hash: binary(),
+          opts: Keyword.t()
         }
 end
