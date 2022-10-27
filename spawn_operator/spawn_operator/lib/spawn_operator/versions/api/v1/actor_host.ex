8e3b178482c086762a2ea03eaf58dd0814cdd55b
Michael Ruoss <michael.ruoss@ufirstgroup.com>
2022-10-27 17:49:53 +0200

    add commented out helper in version manifest

--- spawn_operator/spawn_operator/lib/spawn_operator/versions/api/v1/actor_host.ex
+++ spawn_operator/spawn_operator/lib/spawn_operator/versions/api/v1/actor_host.ex
@@ -2,5 +2,12 @@ defmodule SpawnOperator.Versions.Api.V1.ActorHost do
   use Bonny.API.Version
 
   @impl true
-  def manifest(), do: defaults()
+  def manifest() do
+    defaults()
+    |> struct!(
+      name: "v1",
+      storage: true
+    )
+    # |> add_observed_generation_status()
+  end
 end
