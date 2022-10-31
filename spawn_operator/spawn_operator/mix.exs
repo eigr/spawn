4fc6144c46c3fdb80f14c787243ec02658a8c52d
Adriano Santos <sleipnir@bsd.com.br>
2022-10-31 15:03:41 -0300

    Update bonny

--- spawn_operator/spawn_operator/mix.exs
+++ spawn_operator/spawn_operator/mix.exs
@@ -32,7 +32,7 @@ defmodule Operator.MixProject do
     [
       {:bandit, "~> 0.5"},
       {:bakeware, ">= 0.0.0", runtime: false},
-      {:bonny, "~> 1.0.0-rc.0"},
+      {:bonny, "~> 1.0.0-rc.1"},
       {:spawn, path: "../../"}
     ]
   end
