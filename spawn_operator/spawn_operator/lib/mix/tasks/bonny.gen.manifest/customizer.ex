662a2a33f8119855ee5e3086f990a3012690408b
Adriano Santos <sleipnir@bsd.com.br>
2023-01-21 12:06:40 -0300

    Override Operator bonny generate deployment

--- spawn_operator/spawn_operator/lib/mix/tasks/bonny.gen.manifest/customizer.ex
+++ spawn_operator/spawn_operator/lib/mix/tasks/bonny.gen.manifest/customizer.ex
@@ -19,6 +19,102 @@ defmodule Mix.Tasks.Bonny.Gen.Manifest.SpawnOperatorCustomizer do
 
   @spec override(Bonny.Resource.t()) :: Bonny.Resource.t()
 
+  @doc """
+  This function generated Deployment manifest like bellow:
+
+  ```yaml
+  %{
+    apiVersion: "apps/v1",
+    kind: "Deployment",
+    metadata: %{
+      labels: %{"k8s-app" => "spawn-operator"},
+      name: "spawn-operator",
+      namespace: "default"
+    },
+    spec: %{
+      replicas: 1,
+      selector: %{matchLabels: %{"k8s-app" => "spawn-operator"}},
+      template: %{
+        metadata: %{labels: %{"k8s-app" => "spawn-operator"}},
+        spec: %{
+          containers: [
+            %{
+              env: [
+                %{name: "MIX_ENV", value: "prod"},
+                %{
+                  name: "BONNY_POD_NAME",
+                  valueFrom: %{fieldRef: %{fieldPath: "metadata.name"}}
+                },
+                %{
+                  name: "BONNY_POD_NAMESPACE",
+                  valueFrom: %{fieldRef: %{fieldPath: "metadata.namespace"}}
+                },
+                %{
+                  name: "BONNY_POD_IP",
+                  valueFrom: %{fieldRef: %{fieldPath: "status.podIP"}}
+                },
+                %{
+                  name: "BONNY_POD_SERVICE_ACCOUNT",
+                  valueFrom: %{fieldRef: %{fieldPath: "spec.serviceAccountName"}}
+                }
+              ],
+              image: "eigr/spawn-operator:0.5.0-rc.12",
+              name: "spawn-operator",
+              resources: %{
+                limits: %{cpu: "200m", memory: "200Mi"},
+                requests: %{cpu: "200m", memory: "200Mi"}
+              },
+              securityContext: %{
+                allowPrivilegeEscalation: false,
+                readOnlyRootFilesystem: true,
+                runAsNonRoot: true,
+                runAsUser: 65534
+              },
+              volumeMounts: [
+                %{
+                  "mountPath" => "/app/.cache/bakeware/",
+                  "name" => "bakeware-cache"
+                }
+              ]
+            }
+          ],
+          serviceAccountName: "spawn-operator",
+          volumes: [%{"emptyDir" => %{}, "name" => "bakeware-cache"}]
+        }
+      }
+    }
+  }
+  ```
+  """
+  def override(%{kind: "Deployment"} = resource) do
+    %{resource | spec: %{resource.spec | template: update_template(resource)}}
+  end
+
   # fallback
   def override(resource), do: resource
+
+  defp update_template(resource) do
+    spec = resource.spec.template.spec
+    container = List.first(resource.spec.template.spec.containers)
+
+    updated_spec =
+      Map.put(spec, :volumes, [
+        %{"name" => "bakeware-cache", "emptyDir" => %{}}
+      ])
+
+    updated_container =
+      Map.put(container, :volumeMounts, [
+        %{"mountPath" => "/app/.cache/bakeware/", "name" => "bakeware-cache"}
+      ])
+
+    updated_spec = %{
+      updated_spec
+      | containers: [updated_container]
+    }
+
+    %{
+      resource.spec.template
+      | spec: updated_spec
+    }
+  end
 end
