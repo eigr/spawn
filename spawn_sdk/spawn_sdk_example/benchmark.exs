98b2b331c65aa3fb19a834b8dd3d23ad7d035288
Adriano Santos <sleipnir@bsd.com.br>
2023-01-22 18:22:37 -0300

    Benchmarks without call to sys.get_state

--- spawn_sdk/spawn_sdk_example/benchmark.exs
+++ spawn_sdk/spawn_sdk_example/benchmark.exs
@@ -41,63 +41,64 @@ Benchee.run(
   profile_after: true
 )
 
-Logger.info("Running Non Parallel Invoke - 30s")
+# Logger.info("Running Non Parallel Invoke - 30s")
 
-Benchee.run(
-  %{
-    "Non Parallel Stateful Singleton Actor                  - Get State   " => fn ->
-      invok_get_state()
-    end,
-    "Non Parallel Stateful Singleton Actor                  - Update State" => fn ->
-      invoke_update_state()
-    end,
-    "Non Parallel Stateful Abstract Spawn and Invoke Actor  - Update State" => fn ->
-      spawn_and_invoke()
-    end,
-    "Non Parallel Stateless Pooled Actor                    - Call Action " => fn ->
-      spawn_invoke_pooled_actors()
-    end
-  },
-  time: 30,
-  parallel: 1,
-  formatters: [
-    {
-      Benchee.Formatters.HTML,
-      file: "test/benchmark/results/non-parallel-invocations-30s.html", auto_open: false
-    },
-    Benchee.Formatters.Console
-  ],
-  save: [path: "test/benchmark/results/non-parallel-invocations-30s.benchee"],
-  print: [
-    benchmarking: true,
-    configuration: true,
-    fast_warning: true
-  ],
-  profile_after: true
-)
+# Benchee.run(
+#   %{
+#     "Non Parallel Stateful Singleton Actor                  - Get State   " => fn ->
+#       invok_get_state()
+#     end,
+#     "Non Parallel Stateful Singleton Actor                  - Update State" => fn ->
+#       invoke_update_state()
+#     end,
+#     "Non Parallel Stateful Abstract Spawn and Invoke Actor  - Update State" => fn ->
+#       spawn_and_invoke()
+#     end,
+#     "Non Parallel Stateless Pooled Actor                    - Call Action " => fn ->
+#       spawn_invoke_pooled_actors()
+#     end
+#   },
+#   time: 30,
+#   parallel: 1,
+#   formatters: [
+#     {
+#       Benchee.Formatters.HTML,
+#       file: "test/benchmark/results/non-parallel-invocations-30s.html", auto_open: false
+#     },
+#     Benchee.Formatters.Console
+#   ],
+#   save: [path: "test/benchmark/results/non-parallel-invocations-30s.benchee"],
+#   print: [
+#     benchmarking: true,
+#     configuration: true,
+#     fast_warning: true
+#   ],
+#   profile_after: true
+# )
 
-Logger.info("Running Parallel 10x Invoke - 5s")
+# Logger.info("Running Parallel 10x Invoke - 5s")
 
-#Process.sleep(10000)
-Benchee.run(%{
-  "Non Parallel Stateful Singleton Actor                  - Get State   " => fn -> invok_get_state() end,
-  "Non Parallel Stateful Singleton Actor                  - Update State" => fn -> invoke_update_state() end,
-  "Non Parallel Stateful Abstract Spawn and Invoke Actor  - Update State" => fn -> spawn_and_invoke() end,
-  "Non Parallel Stateless Pooled Actor                    - Call Action " => fn -> spawn_invoke_pooled_actors() end
-  },
-  parallel: 10,
-  formatters: [
-  {
-    Benchee.Formatters.HTML,
-    file: "test/benchmark/results/parallel-invocations-5s.html",
-    auto_open: false
-  },
-    Benchee.Formatters.Console
-  ],
-  save: [path: "test/benchmark/results/parallel-invocations-5s.benchee"],
-  print: [
-    benchmarking: true,
-    configuration: true,
-    fast_warning: true
-  ]
-)
+# #Process.sleep(10000)
+# Benchee.run(%{
+#   "Parallel Stateful Singleton Actor                  - Get State   " => fn -> invok_get_state() end,
+#   "Parallel Stateful Singleton Actor                  - Update State" => fn -> invoke_update_state() end,
+#   #"Parallel Stateful Abstract Spawn and Invoke Actor  - Update State" => fn -> spawn_and_invoke() end,
+#   "Parallel Stateless Pooled Actor                    - Call Action " => fn -> spawn_invoke_pooled_actors() end
+#   },
+#   warmup: 10,
+#   parallel: 10,
+#   formatters: [
+#   {
+#     Benchee.Formatters.HTML,
+#     file: "test/benchmark/results/parallel-invocations-5s.html",
+#     auto_open: false
+#   },
+#     Benchee.Formatters.Console
+#   ],
+#   save: [path: "test/benchmark/results/parallel-invocations-5s.benchee"],
+#   print: [
+#     benchmarking: true,
+#     configuration: true,
+#     fast_warning: true
+#   ]
+# )
