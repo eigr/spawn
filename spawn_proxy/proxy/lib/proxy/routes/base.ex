63a0184e379c9160d2add834852b292b1d45b470
Adriano Santos <sleipnir@bsd.com.br>
2023-02-05 09:53:24 -0300

    Add connection keep-alive

--- spawn_proxy/proxy/lib/proxy/routes/base.ex
+++ spawn_proxy/proxy/lib/proxy/routes/base.ex
@@ -16,6 +16,22 @@ defmodule Proxy.Routes.Base do
 
       plug(:dispatch)
 
+      def send!(conn, code, data, content_type) when is_atom(code) do
+        code =
+          case code do
+            :ok -> 200
+            :not_found -> 404
+            :malformed_data -> 400
+            :non_authenticated -> 401
+            :forbidden_access -> 403
+            :server_error -> 500
+            :service_unavailable -> 503
+            :error -> 504
+          end
+
+        send!(conn, code, data, content_type)
+      end
+
       def send!(conn, code, data, content_type)
           when is_integer(code) and content_type == "application/json" do
         conn
@@ -31,22 +47,6 @@ defmodule Proxy.Routes.Base do
         |> Plug.Conn.put_resp_content_type(content_type)
         |> Plug.Conn.put_resp_header("Connection", "Keep-Alive")
         |> send_resp(code, data)
-      end
-
-      def send!(conn, code, data, content_type) when is_atom(code) do
-        code =
-          case code do
-            :ok -> 200
-            :not_found -> 404
-            :malformed_data -> 400
-            :non_authenticated -> 401
-            :forbidden_access -> 403
-            :server_error -> 500
-            :service_unavailable -> 503
-            :error -> 504
-          end
-
-        send!(conn, code, data, content_type)
       end
     end
   end
