d22dde2d4ca4ee91566adb5810f990a7b165a31c
Adriano Santos <sleipnir@bsd.com.br>
2023-04-22 21:50:53 -0300

    Add come configuration in Nats consumer

--- lib/spawn/cluster/node/server.ex
+++ lib/spawn/cluster/node/server.ex
@@ -1,12 +1,39 @@
 defmodule Spawn.Cluster.Node.Server do
+  @moduledoc """
+  Node subscriber
+  """
   use Gnat.Server
 
-  def request(%{body: _body}) do
-    {:reply, ""}
+  require Logger
+  require OpenTelemetry.Tracer, as: Tracer
+
+  def request(%{topic: topic, body: body, reply_to: reply_to} = req) do
+    Logger.debug("Received Actor Invocation via Nats on #{topic}")
+
+    req
+    |> Map.get(:headers)
+    |> get_trace_context_if_exists()
+
+    topic
+    |> handle_request(body, reply_to)
   end
 
   def error(%{gnat: gnat, reply_to: reply_to}, _error) do
     # TODO handle errors
     # Gnat.pub(gnat, reply_to, "Something went wrong and I can't handle your request")
+  end
+
+  defp handle_request(topic, body, reply_to) do
+    Tracer.with_span "Handle Actor Invoke", kind: :server do
+      {:reply, nil}
+    end
+  end
+
+  defp get_trace_context_if_exists(headers) do
+    if Enum.any?(headers, fn {k, _v} -> k == "traceparent" end) do
+      :otel_propagator_text_map.extract(headers)
+    else
+      OpenTelemetry.Ctx.clear()
+    end
   end
 end
