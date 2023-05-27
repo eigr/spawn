083d7dad062dacbe3609baea16a17b10adc647f7
Adriano Santos <sleipnir@bsd.com.br>
2023-05-27 13:25:11 -0300

    Discard changes

--- spawn_statestores/statestores_cockroachdb/mix.exs
+++ spawn_statestores/statestores_cockroachdb/mix.exs
@@ -2,7 +2,7 @@ defmodule StatestoresCockroachDB.MixProject do
   use Mix.Project
 
   @app :spawn_statestores_cockroachdb
-  @version "0.0.0-local.dev"
+  @version "1.0.0-rc1"
   @source_url "https://github.com/eigr/spawn/blob/main/spawn_statestores/statestores_cockroachdb"
 
   def project do
