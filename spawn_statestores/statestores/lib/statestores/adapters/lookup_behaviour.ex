0d29c950b2678eea30b0bf73353f06dd8eca97eb
Adriano Santos <sleipnir@bsd.com.br>
2023-05-29 11:50:21 -0300

    Change return types

--- spawn_statestores/statestores/lib/statestores/adapters/lookup_behaviour.ex
+++ spawn_statestores/statestores/lib/statestores/adapters/lookup_behaviour.ex
@@ -2,7 +2,7 @@ defmodule Statestores.Adapters.LookupBehaviour do
   @moduledoc """
   `LookupBehaviour` defines how system get clustered actors info.
   """
-  @callback get(any()) :: any()
+  @callback get(any()) :: {:ok, any()} | {:error, any()}
 
   @callback set(any()) :: {:error, any()} | {:ok, any()}
 end
