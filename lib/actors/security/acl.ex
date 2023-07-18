9af011386a3cf0a72d1995d6cdace3970b24c6e7
Adriano Santos <sleipnir@bsd.com.br>
2023-07-18 20:15:22 -0300

    Initial implementation of access control list feature

---
+++ lib/actors/security/acl.ex
@@ -0,0 +1,27 @@
+defmodule Actors.Security.Acl do
+  @moduledoc """
+  `Acl` is a acces control list helper module
+  """
+  alias Eigr.Functions.Protocol.InvocationRequest
+
+  @type base_policies_path :: String.t()
+  @type invocation :: InvocationRequest.t()
+  @type policy :: Policy.t()
+  @type policies :: list(AclPolicy.t())
+
+  @callback load_acl_policies(base_policies_path()) :: policies()
+
+  @callback is_authorized?(policy(), invocation()) :: boolean()
+
+  defmodule Policy do
+    defstruct name: nil, type: nil, actors: nil, actor_systems: nil, actions: nil
+
+    @type t :: %__MODULE__{
+            name: String.t(),
+            type: String.t() | atom(),
+            actors: list(String.t()),
+            actor_systems: list(String.t()),
+            actions: list(String.t())
+          }
+  end
+end
---
+++ lib/actors/security/acl/default_acl_manager.ex
@@ -0,0 +1,89 @@
+defmodule Actors.Security.Acl.DefaultAclManager do
+  @doc """
+  `DefaultAclManager`
+  """
+  alias Actors.Security.Acl.Policy
+
+  import Spawn.Utils.Common, only: [to_existing_atom_or_new: 1]
+
+  @behaviour Actors.Security.Acl
+
+  @impl true
+  def load_acl_policies(base_policies_path) do
+    case Agent.start_link(fn -> %{} end, name: __MODULE__) do
+      {:ok, _pid} ->
+        Agent.get_and_update(__MODULE__, fn policies ->
+          update_policies(base_policies_path, policies)
+        end)
+
+      {:error, {:already_started, _pid}} ->
+        Agent.get(__MODULE__, fn policies -> policies end)
+    end
+  end
+
+  @impl true
+  def is_authorized?(%Policy{} = _policy, _invocation) do
+    true
+  end
+
+  def load_policies(base_policies_path) do
+    Path.wildcard("#{base_policies_path}/*.policy")
+    |> Enum.map(fn file -> {file, from_file_to_map(file)} end)
+    |> Enum.map(fn {file, data} ->
+      %Policy{
+        name: get_file_name(file),
+        type: Map.get(data, :type, :allow),
+        actors: Map.get(data, :actors, ["*"]),
+        actions: Map.get(data, :actions, ["*"]),
+        actor_systems: Map.get(data, :actor_systems, ["*"])
+      }
+    end)
+  end
+
+  defp from_file_to_map(path) do
+    if File.exists?(path) do
+      File.stream!(path)
+      |> Stream.map(fn line ->
+        [key | value] = String.split(line, ":")
+        key = to_existing_atom_or_new(key)
+
+        value =
+          case key do
+            :type ->
+              List.first(value)
+              |> String.replace("\n", "")
+              |> String.trim()
+              |> String.downcase()
+              |> to_existing_atom_or_new()
+
+            _ ->
+              value
+              |> Enum.map(fn elem ->
+                String.replace(elem, "\n", "")
+                |> String.trim()
+              end)
+          end
+
+        {key, value}
+      end)
+      |> Enum.into(%{})
+    else
+      %{}
+    end
+  end
+
+  defp get_file_name(file) do
+    Path.basename(file)
+    |> String.replace(".policy", "")
+    |> String.trim()
+  end
+
+  defp update_policies(path, policies) do
+    if policies == %{} do
+      policies = load_policies(path)
+      {policies, policies}
+    else
+      {policies, policies}
+    end
+  end
+end
