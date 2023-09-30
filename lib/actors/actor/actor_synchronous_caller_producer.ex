93021e418d11472133b0a67f7c7f20a715b1c10a
Adriano Santos <sleipnir@bsd.com.br>
2023-09-30 16:30:45 -0300

    Fix timeout

--- lib/actors/actor/actor_synchronous_caller_producer.ex
+++ lib/actors/actor/actor_synchronous_caller_producer.ex
@@ -1,10 +1,49 @@
 defmodule Actors.Actor.ActorSynchronousCallerProducer do
   use GenStage
 
+  alias Eigr.Functions.Protocol.Actors.ActorId
+
+  alias Eigr.Functions.Protocol.{
+    InvocationRequest,
+    RegistrationRequest,
+    RegistrationResponse,
+    SpawnRequest,
+    SpawnResponse
+  }
+
+  # Client API
+
   @spec start_link(any) :: :ignore | {:error, any} | {:ok, pid}
   def start_link(state \\ []) do
     GenStage.start_link(__MODULE__, state, name: __MODULE__)
   end
+
+  @spec get_state(ActorId.t()) :: {:ok, term()} | {:error, term()}
+  def get_state(actor_id, opts \\ []) do
+    GenStage.call(__MODULE__, {:enqueue, {:get_state, actor_id, opts}}, :infinity)
+  end
+
+  @spec register(RegistrationRequest.t(), any()) ::
+          {:ok, RegistrationResponse.t()} | {:error, RegistrationResponse.t()}
+  def register(registration, opts \\ []) do
+    GenStage.call(__MODULE__, {:enqueue, {:register, registration, opts}}, :infinity)
+  end
+
+  @spec spawn_actor(SpawnRequest.t(), any()) :: {:ok, SpawnResponse.t()}
+  def spawn_actor(spawn_req, opts \\ []) do
+    GenStage.call(__MODULE__, {:enqueue, {:spawn_actor, spawn_req, opts}}, :infinity)
+  end
+
+  @spec invoke(InvocationRequest.t()) :: {:ok, :async} | {:ok, term()} | {:error, term()}
+  def invoke(request, opts \\ []) do
+    GenStage.call(__MODULE__, {:enqueue, {:invoke, request, opts}}, :infinity)
+  end
+
+  def enqueue(event) do
+    GenStage.call(__MODULE__, {:enqueue, event})
+  end
+
+  # Server API
 
   def init(_state) do
     {:producer, {:queue.new(), 0}}
@@ -17,26 +56,6 @@ defmodule Actors.Actor.ActorSynchronousCallerProducer do
 
   def handle_demand(incoming_demand, {queue, pending_demand}) do
     dispatch_events(queue, incoming_demand + pending_demand, [])
-  end
-
-  def get_state(actor_id, opts \\ []) do
-    GenStage.call(__MODULE__, {:enqueue, {:get_state, actor_id, opts}})
-  end
-
-  def register(registration, opts \\ []) do
-    GenStage.call(__MODULE__, {:enqueue, {:register, registration, opts}})
-  end
-
-  def spawn_actor(spawn_req, opts \\ []) do
-    GenStage.call(__MODULE__, {:enqueue, {:spawn_actor, spawn_req, opts}})
-  end
-
-  def invoke(request, opts \\ []) do
-    GenStage.call(__MODULE__, {:enqueue, {:invoke, request, opts}})
-  end
-
-  def enqueue(event) do
-    GenStage.call(__MODULE__, {:enqueue, event})
   end
 
   defp dispatch_events(queue, 0, events) do
