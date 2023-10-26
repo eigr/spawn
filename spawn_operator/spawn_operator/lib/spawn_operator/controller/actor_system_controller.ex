8697bf7299590432370bfff2f00c74a3c5f6ea91
Paulo Henrique Alves <paulohenriqueas13@gmail.com>
2023-10-26 15:37:52 -0300

    Fix to deploy on Openshift by adding resources to plural.

--- spawn_operator/spawn_operator/lib/spawn_operator/controller/actor_system_controller.ex
+++ spawn_operator/spawn_operator/lib/spawn_operator/controller/actor_system_controller.ex
@@ -8,10 +8,10 @@ defmodule SpawnOperator.Controller.ActorSystemController do
 
   def rbac_rules() do
     [
-      to_rbac_rule({"rbac.authorization.k8s.io", "role", "*"}),
-      to_rbac_rule({"", "serviceaccount", "*"}),
+      to_rbac_rule({"rbac.authorization.k8s.io", ["role", "roles", "rolebindings"], "*"}),
+      to_rbac_rule({"", ["serviceaccount", "serviceaccounts"], "*"}),
       to_rbac_rule({"", "pods", "*"}),
-      to_rbac_rule({"", "node", ["get", "list"]}),
+      to_rbac_rule({"", ["node", "nodes"], ["get", "list"]}),
       to_rbac_rule({"apps", "deployments", "*"}),
       to_rbac_rule({"", "secrets", "*"}),
       to_rbac_rule({"", ["services", "configmaps"], "*"}),
