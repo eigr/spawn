7f85dcabd1a3c85a9c1f84a948bb34f87c8ba041
Adriano Santos <sleipnir@bsd.com.br>
2023-11-06 12:18:33 -0300

    Fix. Spawn on GenStage consumer side

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-version=1.0.0-rc.20
+version=1.0.0-rc.21
 registry=eigr
 
 CLUSTER_NAME=spawn-k8s
--- lib/actors/actor/caller_consumer.ex
+++ lib/actors/actor/caller_consumer.ex
@@ -125,7 +125,7 @@ defmodule Actors.Actor.CallerConsumer do
       protocol_major_version: 1,
       protocol_minor_version: 2,
       proxy_name: "spawn",
-      proxy_version: "1.0.0-rc.20"
+      proxy_version: "1.0.0-rc.21"
     }
   end
 
