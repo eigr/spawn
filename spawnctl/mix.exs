632cfa37680d41b22622b8c951bc528bc494e3da
Adriano Santos <sleipnir@bsd.com.br>
2024-08-06 21:30:30 -0300

    Remove unused dependency

--- spawnctl/mix.exs
+++ spawnctl/mix.exs
@@ -32,7 +32,6 @@ defmodule SpawnCtl.MixProject do
     [
       {:burrito, "~> 1.1"},
       {:do_it, "~> 0.6"},
-      {:earmark, "~> 1.4"},
       {:exmoji, "~> 0.3"},
       {:file_system, "~> 1.0"},
       {:jason, "~> 1.4"},
