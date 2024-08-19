c3227c7edff829786482fabe9408ffddf26d3f0f
Adriano Santos <sleipnir@bsd.com.br>
2024-08-19 20:49:11 -0300

    fix: host and system commands

--- spawnctl/lib/spawnctl/commands/config/host.ex
+++ spawnctl/lib/spawnctl/commands/config/host.ex
@@ -5,4 +5,7 @@ defmodule Spawnctl.Commands.Config.Host do
   use DoIt.Command,
     name: "host",
     description: "Configue Host application via ActorHost CRD."
+
+  def run(_, _opts, _context) do
+  end
 end
--- spawnctl/lib/spawnctl/commands/config/system.ex
+++ spawnctl/lib/spawnctl/commands/config/system.ex
@@ -5,4 +5,7 @@ defmodule Spawnctl.Commands.Config.System do
   use DoIt.Command,
     name: "system",
     description: "Configue ActorSystem via ActorSystem CRD."
+
+  def run(_, _opts, _context) do
+  end
 end
