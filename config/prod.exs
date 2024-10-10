1913a16515e5e6903d56d0bb00b626b73bde066a
Adriano Santos <solid.sistemas@gmail.com>
2024-10-10 06:37:21 -0300

    Fix typo

--- config/prod.exs
+++ config/prod.exs
@@ -4,4 +4,4 @@ config :do_it, DoIt.Commfig,
   dirname: System.tmp_dir(),
   filename: "spawn_cli.json"
 
-config :flame, :terminator, failsafe_timeout: :timer.minuts(1)
+config :flame, :terminator, failsafe_timeout: :timer.minutes(1)
