f2f393a5ac8876b01ac7af474c5ba5ca83f35351
Adriano Santos <adriano.santos@v3.com.br>
2024-12-25 22:09:31 -0300

    feat: add support to group by

--- spawn_statestores/statestores/lib/statestores/projection/query/query_builder.ex
+++ spawn_statestores/statestores/lib/statestores/projection/query/query_builder.ex
@@ -5,109 +5,98 @@ defmodule Statestores.Projection.Query.QueryBuilder do
   """
 
   @spec build_query(list(), list(), list(), map()) :: {String.t(), list()}
-  def build_query(select_clause, conditions, order_by, _binds) do
+  def build_query(select_clause, conditions, order_by, group_by) do
     select_sql =
       select_clause
       |> Enum.filter(&valid_select?/1)
-      |> Enum.map(fn
-        :count_star ->
-          "COUNT(*)"
-
-        {:avg, attr, _opts} ->
-          "AVG(tags->>'#{attr}')::numeric"
-
-        {:min, attr} ->
-          "MIN(tags->>'#{attr}')::numeric"
-
-        {:min, attr, _opts} ->
-          "MIN(tags->>'#{attr}')::numeric"
-
-        {:max, attr} ->
-          "MAX(tags->>'#{attr}')::numeric"
-
-        {:max, attr, _opts} ->
-          "MAX(tags->>'#{attr}')::numeric"
-
-        {:sum, attr} ->
-          "SUM(tags->>'#{attr}')::numeric"
-
-        {:sum, attr, _opts} ->
-          "SUM(tags->>'#{attr}')::numeric"
-
-        {:rank_over, attr, dir} ->
-          "RANK() OVER (ORDER BY (tags->>'#{attr}')::numeric #{String.upcase(to_string(dir))})"
-
-        attr when is_atom(attr) ->
-          "tags->>'#{attr}' AS #{attr}"
-
-        _ ->
-          raise ArgumentError, "Unsupported select clause format"
-      end)
+      |> Enum.map(&build_select_clause/1)
       |> Enum.join(", ")
 
     where_sql = build_where_clause(conditions)
     order_by_sql = build_order_by_clause(order_by)
 
+    group_by_sql =
+      case group_by do
+        %{} ->
+          ""
+
+        [] ->
+          ""
+
+        nil ->
+          ""
+
+        _ ->
+          if valid_group_by?(group_by, select_clause) do
+            build_group_by_clause(group_by)
+          else
+            raise(
+              ArgumentError,
+              "GROUP BY must be used in conjunction with aggregation functions"
+            )
+          end
+      end
+
     query =
-      ["SELECT", select_sql, "FROM projections", where_sql, order_by_sql]
+      if group_by_sql == "" do
+        ["SELECT", select_sql, "FROM projections", where_sql, order_by_sql]
+      else
+        ["SELECT", select_sql, "FROM projections", where_sql, group_by_sql, order_by_sql]
+      end
       |> Enum.reject(&(&1 == ""))
       |> Enum.join(" ")
 
     {String.trim(query), []}
   end
 
+  defp build_select_clause(:count_star), do: "COUNT(*)"
+
+  defp build_select_clause({:avg, attr, _opts}), do: "AVG(tags->>'#{attr}')::numeric"
+
+  defp build_select_clause({:min, attr, _opts}), do: "MIN(tags->>'#{attr}')::numeric"
+
+  defp build_select_clause({:max, attr, _opts}), do: "MAX(tags->>'#{attr}')::numeric"
+
+  defp build_select_clause({:sum, attr, _opts}), do: "SUM(tags->>'#{attr}')::numeric"
+
+  defp build_select_clause({:rank_over, attr, dir}),
+    do: "RANK() OVER (ORDER BY (tags->>'#{attr}')::numeric #{String.upcase(to_string(dir))})"
+
+  defp build_select_clause({:subquery, subquery, alias_name}),
+    do: "(#{subquery}) AS #{alias_name}"
+
+  defp build_select_clause(attr) when is_atom(attr),
+    do: "tags->>'#{attr}' AS #{attr}"
+
+  defp build_select_clause(_),
+    do: raise(ArgumentError, "Unsupported select clause format")
+
   defp build_where_clause([]), do: ""
 
   defp build_where_clause(conditions) do
     conditions
-    |> Enum.map(fn
-      {:where, field, operator, value} ->
-        build_condition(field, operator, value)
-
-      _ ->
-        ""
-    end)
+    |> Enum.map(&build_condition/1)
     |> Enum.reject(&(&1 == ""))
     |> Enum.join(" AND ")
     |> (&("WHERE " <> &1)).()
   end
 
-  defp build_condition(field, operator, value) when is_tuple(value) do
+  defp build_condition({:where, field, operator, value}) when is_tuple(value) do
     subquery = build_subquery(value)
     "#{field} #{operator} (#{subquery})"
   end
 
-  defp build_condition(field, operator, value) do
+  defp build_condition({:where, field, operator, value}) do
     formatted_value =
-      case value do
-        ^value when is_binary(value) -> "'#{String.trim(value, "'")}'"
-        _ -> value
-      end
+      if is_binary(value), do: "'#{String.trim(value, "'")}'", else: value
 
     "#{field} #{operator} #{formatted_value}"
   end
 
   defp build_subquery({:select, select_clause, where_clause, order_by_clause}) do
     select_sql =
-      Enum.map(select_clause, fn
-        :count_star ->
-          "COUNT(*)"
-
-        {:avg, field} ->
-          "AVG(tags->>'#{field}')::numeric"
-
-        {:min, field} ->
-          "MIN(tags->>'#{field}')::numeric"
-
-        {:max, field} ->
-          "MAX(tags->>'#{field}')::numeric"
-
-        {:rank_over, attr, dir} ->
-          "RANK() OVER (ORDER BY (tags->>'#{attr}')::numeric #{String.upcase(to_string(dir))})"
-
-        attr ->
-          "tags->>'#{attr}' AS #{attr}"
-      end)
+      select_clause
+      |> Enum.map(&build_select_clause/1)
       |> Enum.join(", ")
 
     where_sql = build_where_clause(where_clause)
@@ -130,15 +119,33 @@ defmodule Statestores.Projection.Query.QueryBuilder do
     |> (&("ORDER BY " <> &1)).()
   end
 
+  defp build_group_by_clause([]), do: ""
+
+  defp build_group_by_clause(group_by) do
+    group_by
+    |> Enum.map(&to_string/1)
+    |> Enum.join(", ")
+    |> (&("GROUP BY " <> &1)).()
+  end
+
   defp valid_select?(:count_star), do: true
   defp valid_select?({:avg, _attr, _opts}), do: true
-  defp valid_select?({:min, _attr}), do: true
   defp valid_select?({:min, _attr, _opts}), do: true
-  defp valid_select?({:max, _attr}), do: true
   defp valid_select?({:max, _attr, _opts}), do: true
-  defp valid_select?({:sum, _attr}), do: true
   defp valid_select?({:sum, _attr, _opts}), do: true
   defp valid_select?({:rank_over, _attr, _dir}), do: true
+  defp valid_select?({:subquery, _query, _alias_name}), do: true
   defp valid_select?(attr) when is_atom(attr), do: true
   defp valid_select?(_), do: false
+
+  defp valid_group_by?(group_by, select_clause) do
+    Enum.any?(select_clause, &aggregation_function?/1)
+  end
+
+  defp aggregation_function?(:count_star), do: true
+  defp aggregation_function?({:avg, _, _}), do: true
+  defp aggregation_function?({:min, _, _}), do: true
+  defp aggregation_function?({:max, _, _}), do: true
+  defp aggregation_function?({:sum, _, _}), do: true
+  defp aggregation_function?(_), do: false
 end
--- spawn_statestores/statestores/test/projection_query_builder_test.exs
+++ spawn_statestores/statestores/test/projection_query_builder_test.exs
@@ -119,4 +119,70 @@ defmodule Projection.Query.BuilderTest do
       assert query == expected_query
     end
   end
+
+  describe "build_query/4 with complex queries" do
+    test "builds query with group by clause" do
+      select_clause = [{:sum, :field1, %{}}]
+      conditions = []
+      order_by = []
+      group_by = [:field1]
+
+      {query, _params} = QueryBuilder.build_query(select_clause, conditions, order_by, group_by)
+
+      expected_query = "SELECT SUM(tags->>'field1')::numeric FROM projections GROUP BY field1"
+
+      assert query == expected_query
+    end
+
+    test "builds query with group by and aggregate functions" do
+      select_clause = [
+        {:sum, :field1, %{}},
+        {:avg, :field2, %{}}
+      ]
+
+      conditions = []
+      order_by = []
+      group_by = [:field1]
+
+      {query, _params} = QueryBuilder.build_query(select_clause, conditions, order_by, group_by)
+
+      expected_query =
+        "SELECT SUM(tags->>'field1')::numeric, AVG(tags->>'field2')::numeric FROM projections GROUP BY field1"
+
+      assert query == expected_query
+    end
+
+    test "build_query/4 builds a query with subquery in SELECT clause" do
+      select_clause = [
+        :player_id,
+        :points,
+        {:subquery, "SELECT COUNT(*) FROM projections AS t2 WHERE t2.points > t1.points", "rank"}
+      ]
+
+      conditions = []
+      order_by = []
+
+      {query, _params} = QueryBuilder.build_query(select_clause, conditions, order_by, %{})
+
+      expected_query =
+        "SELECT tags->>'player_id' AS player_id, tags->>'points' AS points, (SELECT COUNT(*) FROM projections AS t2 WHERE t2.points > t1.points) AS rank FROM projections"
+
+      assert query == expected_query
+    end
+  end
+
+  describe "build_query/4 with invalid GROUP BY" do
+    test "raises an exception when GROUP BY is used without an aggregation function" do
+      select_clause = [:some_field]
+      conditions = []
+      order_by = []
+      group_by = [:some_field]
+
+      assert_raise ArgumentError,
+                   ~r/GROUP BY must be used in conjunction with aggregation functions/,
+                   fn ->
+                     QueryBuilder.build_query(select_clause, conditions, order_by, group_by)
+                   end
+    end
+  end
 end
